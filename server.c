a125dcf75ec7b29fcb1f148a45ed0ed48ad5bd67
Nicholas <nicholasbarsanti000205@gmail.com>
2025-06-13 09:56:50 +0000

    Creazione ed implementazione file server.c

---
+++ server.c
@@ -0,0 +1,118 @@
+// server/server.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUF_SIZE 1024
+
+
+void createSocket(int *server_fd);
+void configureAddress(struct sockaddr_in *address);
+void binding(int server_fd, struct sockaddr *address, socklen_t addrlen);
+void acceptConnections(int server_fd, int *new_socket);
+
+
+int main(){
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUF_SIZE] = {0};
+
+    // creazione socket ipv4 e socket TCP
+    createSocket(&server_fd);
+
+    // configurazione dell'indirizzo
+    configureAddress(&address);
+
+    // Binding del socket ad un indirizzo e porta specifici
+    binding(server_fd, (struct sockaddr *)&address, sizeof(address));
+
+    // Ascolta le connessioni in arrivo
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server in ascolto sulla porta %d...\n", PORT);
+
+    // Accetta una connessione in arrivo
+    acceptConnections(server_fd, &new_socket);
+    
+    // Riceve dati dal client
+    int bytes_read = read(new_socket, buffer, BUF_SIZE);
+    if (bytes_read < 0) {
+        perror("Read failed");
+        close(new_socket);
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Messaggio ricevuto: %s\n", buffer);
+
+    // Invia una risposta al client
+    const char *response = "Messaggio ricevuto con successo!";
+    if (send(new_socket, response, strlen(response), 0) < 0) {
+        perror("Send failed");
+        close(new_socket);
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Risposta inviata al client.\n");
+
+    // Chiude il socket del client
+    close(new_socket);
+    // Chiude il socket del server
+    close(server_fd);
+    printf("Server chiuso.\n");
+
+    return 0;
+}
+
+void createSocket(int *server_fd){
+    *server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*server_fd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // per evitare errori di "Address already in use" se riavvii il server rapidamente
+    if (setsockopt(*server_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+        perror("Set socket options failed");
+        close(*server_fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void configureAddress(struct sockaddr_in *address) {
+    int opt = 1;
+    address->sin_family = AF_INET; // IPv4
+    address->sin_addr.s_addr = INADDR_ANY; // Accetta connessioni da qualsiasi indirizzo
+    address->sin_port = htons(PORT); // Imposta la porta
+}
+
+void binding(int server_fd, struct sockaddr *address, socklen_t addrlen) {
+    if (bind(server_fd, address, addrlen) < 0) {
+        perror("Bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void acceptConnections(int server_fd, int *new_socket) {
+    struct sockaddr_in client_address;
+    socklen_t client_len = sizeof(client_address);
+
+    *new_socket = accept(server_fd, (struct sockaddr *)&client_address, &client_len);
+    if (*new_socket < 0) {
+        perror("Errore in accept");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connessione accettata da %s:%d\n",
+           inet_ntoa(client_address.sin_addr),
+           ntohs(client_address.sin_port));
+}
