a0aa9ca91b5f2505c7cc5376f07ce8b8066cf38f
Nicholas <nicholasbarsanti000205@gmail.com>
2025-06-20 14:37:02 +0000

    Implelemtata ricerca relativa a user loggato per campi

--- Ticketing-System/Client/menu_client.c
+++ Ticketing-System/Client/menu_client.c
@@ -4,68 +4,100 @@
 #include <sys/socket.h>
 #include "menu_client.h"
 
-// buildTicketMessage è definita in client.c, la referenziamo qui
 extern void buildTicketMessage(char *dest, int max_length);
 
+void sendRequestaAndReveiveResponse(int client_fd, const char *messaggio, char *buffer, size_t bufsize);
+
 void avviaMenuClient(int client_fd, const char *username) {
-    char scelta[10], messaggio[512];
+    char scelta[10], messaggio[512], response[8192];
 
     while (1) {
         printf("\n--- MENU CLIENT ---\n");
         printf("1. Inserisci un nuovo ticket\n");
-        printf("2. Visualizza tutti i ticket per l'utente loggato\n");
-        printf("3. Cerca un ticket dell'utente loggato per ID\n");
+        printf("2. Visualizza tutti i tuoi ticket\n");
+        printf("3. Cerca un tuo ticket per ID\n");
+        printf("4. Cerca tuoi ticket per titolo\n");
+        printf("5. Cerca tuoi ticket per descrizione\n");
+        printf("6. Cerca tuoi ticket per stato (Aperto/In Corso/Chiuso)\n");
         printf("0. Esci\n");
         printf("Scelta: ");
         fgets(scelta, sizeof(scelta), stdin);
-    
-        if (scelta[0] == '1') {
-            printf("\nInserimento nuovo ticket:\n");
-            memset(messaggio, 0, sizeof(messaggio));
-            buildTicketMessage(messaggio, sizeof(messaggio));
-            send(client_fd, messaggio, strlen(messaggio), 0);
 
-            char risposta[2048] = {0};
-            recv(client_fd, risposta, sizeof(risposta) - 1, 0);
-            printf("Risposta: %s\n", risposta);
-
-        } else if (scelta[0] == '2') {
-            printf("\nVisualizzazione di tutti i ticket associati all'utente: \n");
-            snprintf(messaggio, sizeof(messaggio), "GET_ALL_TICKETS_BY_USER|%s", username);
-            send(client_fd, messaggio, strlen(messaggio), 0);
-
-            char risposta[8192] = {0};
-            int bytes = recv(client_fd, risposta, sizeof(risposta) - 1, 0);
-            if (bytes > 0) {
-                risposta[bytes] = '\0';
-                printf("%s\n", risposta);
+        switch(scelta[0]){
+            case '1': {
+                printf("\nInserimento nuovo ticket:\n");
+                memset(messaggio, 0, sizeof(messaggio));
+                buildTicketMessage(messaggio, sizeof(messaggio));
+                sendRequestaAndReveiveResponse(client_fd, messaggio, response, sizeof(response));
+                break;
             }
 
-        } else if (scelta[0] == '3') {
-            printf("\nCerca un ticket per ID: \n");
-            char id_input[10];
-            printf("Inserisci ID: ");
-            fgets(id_input, sizeof(id_input), stdin);
-            id_input[strcspn(id_input, "\n")] = 0;
-
-            snprintf(messaggio, sizeof(messaggio), "GET_TICKET_BY_ID_AND_USER|%s|%s", id_input, username);
-            send(client_fd, messaggio, strlen(messaggio), 0);
-
-
-            char risposta[1024] = {0};
-            int bytes = recv(client_fd, risposta, sizeof(risposta) - 1, 0);
-            if (bytes > 0) {
-                risposta[bytes] = '\0';
-                printf("%s\n", risposta);
+            case '2': {
+                snprintf(messaggio, sizeof(messaggio), "GET_ALL_TICKETS_BY_USER|%s", username);
+                sendRequestaAndReveiveResponse(client_fd, messaggio, response, sizeof(response));
+                break;
             }
 
-        } else if (scelta[0] == '0') {
-            printf("Uscita...\n");
-            break;
-        } else {
-            printf("Scelta non valida.\n");
+            case '3': {
+                char id_input[10];
+                printf("Inserisci ID: ");
+                fgets(id_input, sizeof(id_input), stdin);
+                id_input[strcspn(id_input, "\n")] = 0;
+                snprintf(messaggio, sizeof(messaggio), "GET_TICKET_BY_ID_AND_USER|%s|%s", id_input, username);
+                sendRequestaAndReveiveResponse(client_fd, messaggio, response, sizeof(response));
+                break;
+            }
+
+            case '4': {
+                char titolo[100];
+                printf("Inserisci una parola contenuta nel titolo: ");
+                fgets(titolo, sizeof(titolo), stdin);
+                titolo[strcspn(titolo, "\n")] = 0;
+                snprintf(messaggio, sizeof(messaggio), "GET_TICKET_BY_TITOLO_BY_USER|%s|%s", titolo, username);
+                sendRequestaAndReveiveResponse(client_fd, messaggio, response, sizeof(response));
+                break;
+            }
+
+            case '5': {
+                char descrizione[200];
+                printf("Inserisci una parola nella descrizione: ");
+                fgets(descrizione, sizeof(descrizione), stdin);
+                descrizione[strcspn(descrizione, "\n")] = 0;
+                snprintf(messaggio, sizeof(messaggio), "GET_TICKET_BY_DESCRIZIONE_BY_USER|%s|%s", descrizione, username);
+                sendRequestaAndReveiveResponse(client_fd, messaggio, response, sizeof(response));
+                break;
+            }
+
+            case '6': {
+                char stato[32];
+                printf("Inserisci stato (Aperto, In Corso, Chiuso): ");
+                fgets(stato, sizeof(stato), stdin);
+                stato[strcspn(stato, "\n")] = 0;
+                snprintf(messaggio, sizeof(messaggio), "GET_TICKET_BY_STATO_BY_USER|%s|%s", stato, username);
+                sendRequestaAndReveiveResponse(client_fd, messaggio, response, sizeof(response));
+                break;
+            }
+
+            case '0':
+                printf("Uscita...\n");
+                close(client_fd);
+                return;
+
+            default:
+                printf("Scelta non valida.\n");
+                break;
         }
     }
+}
 
-    close(client_fd);
+void sendRequestaAndReveiveResponse(int client_fd, const char *messaggio, char *buffer, size_t bufsize) {
+    send(client_fd, messaggio, strlen(messaggio), 0);
+    memset(buffer, 0, bufsize);
+    int bytes = recv(client_fd, buffer, bufsize - 1, 0);
+    if (bytes > 0) {
+        buffer[bytes] = '\0';
+        printf("%s\n", buffer);
+    } else {
+        printf("Errore durante la ricezione della risposta dal server.\n");
+    }
 }
